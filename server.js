a3932276383945f6816ec195962ba8f9589d0c2e
RushiRuns <rushikeshaundhakar715@gmail.com>
2026-04-10 09:05:25 +0530

    solved CORS issue

--- server.js
+++ server.js
@@ -151,6 +151,8 @@ app.post("/api/login", (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000,
+    sameSite: "none",
+    secure: true,
   });
   res.json({
     token,
